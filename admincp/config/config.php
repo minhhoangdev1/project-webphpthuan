d7bd02a3f4ccba73b6503c7cb12f666201c35318
Tran Minh Hoang <110228288+minhhoangdev1@users.noreply.github.com>
2022-09-14 15:14:04 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,5 +1,7 @@
 <?php
+    header("Content-type: text/html; charset=utf-8");
     $mysqli = new mysqli("localhost","root","","web_phpthuan");
+    mysqli_set_charset($mysqli, 'UTF8');
     // Check connection
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
